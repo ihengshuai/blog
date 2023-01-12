e5f2d2288c1b0f54636876fcf254dc0d2db052c8
ihengshuai <wsm_1105@163.com>
2023-01-12 07:08:20 +0000

    update page

---
+++ pwa-worker.js
@@ -0,0 +1,15 @@
+// worker.js
+self.addEventListener("install", (event) => {
+    console.log("install")
+    event.waitUnit(self.skipWaiting())
+})
+
+self.addEventListener("activate", (event) => {
+    console.log("activate")
+    event.waitUnit(self.clients.claim())
+})
+
+
+self.addEventListener("fetch", (event) => {
+    console.log("fetch..")
+})
